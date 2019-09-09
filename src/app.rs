bab039d8fb5619a0d4cdb05a502e5cf50d204344
Mauricio Muratori <maurimura@gmail.com>
2019-09-09 20:31:38 -0300

    Getting value from input! Know how mutate state

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,42 @@
-use yew::prelude::*;
+use yew::services::ConsoleService;
+use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
 
-pub struct App {}
+// Here's were the state goes.
+pub struct App {
+    value: i64,
+    console: ConsoleService,
+    input: String
+}
 
-pub enum Msg {}
+pub enum Msg {
+    DoIt,
+    Input(String)
+}
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        App {}
+        App {
+            console: ConsoleService::new(),
+            value: 0,
+            input: String::from("")
+        }
     }
 
     fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        match _msg {
+            Msg::DoIt => {
+                // Update your model on events
+                self.value = self.value + 1;
+                self.console.log("+1 bitch");
+            }
+
+            Msg::Input(val) => {
+                self.input = val 
+            }
+        }
         true
     }
 }
@@ -20,7 +44,12 @@ impl Component for App {
 impl Renderable<App> for App {
     fn view(&self) -> Html<Self> {
         html! {
-            <p>{ "Hello Mauri!" }</p>
+            <div>
+                <p>{ self.value }</p>
+                <button onclick=|_| Msg::DoIt>{ "Click me!" }</button>
+                <input id="input" oninput=|e| Msg::Input(e.value)/>
+                <p>{&self.input}</p>
+            </div>
         }
     }
 }
